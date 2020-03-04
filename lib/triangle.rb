4e4be757d216cba2b83403e7ad6949bdc2e01db4
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-04 07:40:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,8 @@ end
 def kind 
  if (@side_a == @side_b) && (@side_b == @side_c) 
     :equilateral
-  elsif @side_a == @side_b && @side_a !== @side_c || @side_a == @side_c && @side_a !== @side_b || @side_c == @side_b && @side_b !== @side_a
-   return  :isosceles
+  elsif @side_a == @side_b || @side_a == @side_c || @side_c == @side_b 
+    :isosceles
  else @side_a !== @side_b && @side_a !== @side_c && @side_b !== @side_c
    return :scalene
  end 
