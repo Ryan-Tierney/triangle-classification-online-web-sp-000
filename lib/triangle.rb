6eae379f85fed2476c857ef0c4315087db92554c
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-04 07:47:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ end
 def kind 
   if (@side_a <= 0) || (@side_b <= 0) || (@side_c <= 0)
     raise TriangleError
-  else (@side_a + @side_b <= @side_c) || (@side_a + @side_c <= @side_b) || (@side_b + @side_c <= @side_a)
+  elsif (@side_a + @side_b <= @side_c) || (@side_a + @side_c <= @side_b) || (@side_b + @side_c <= @side_a)
     raise TriangleError
-  end 
+  else 
  if (@side_a == @side_b) && (@side_b == @side_c) 
     :equilateral
   elsif (@side_a == @side_b) || (@side_a == @side_c) || (@side_c == @side_b) 
