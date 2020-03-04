d25502b16d6a910d9f6ad715061413418bbed9f0
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-03-04 07:43:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,7 @@ def initialize(side_a, side_b, side_c)
 end 
 
 def kind 
+  
  if (@side_a == @side_b) && (@side_b == @side_c) 
     :equilateral
   elsif (@side_a == @side_b) || (@side_a == @side_c) || (@side_c == @side_b) 
